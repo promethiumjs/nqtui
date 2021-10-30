9675d8e98e77682ecba19514db81cd32617b697f
paglobal <jackamoah.pa@gmail.com>
2021-10-30 23:32:32 +0000

    added support for custom component events

--- package/src/Component.js
+++ package/src/Component.js
@@ -3,12 +3,15 @@ import { render } from "lit-html";
 export default class Component {
   constructor(props) {
     this.state = {};
+    this.events = {};
     this.children = {};
     this.props = props || {};
   }
 
   static create(ClassName, props) {
-    return new ClassName(props);
+    let component = new ClassName(props);
+    if (component.addChildNodes) component.addChildNodes();
+    return component;
   }
 
   static createInstanceFromObject(ClassName, object) {
@@ -18,19 +21,42 @@ export default class Component {
   addChild(ClassName, nodeName, props) {
     this.children[nodeName] = new ClassName(props);
 
+    if (this.events.hasOwnProperty("updateRoot")) {
+      this.children[nodeName].subscribeToEvent(
+        "updateRoot",
+        this.events.updateRoot[0]
+      );
+    }
+
+    if (this.children[nodeName].addChildNodes)
+      this.children[nodeName].addChildNodes();
+
     return this.children[nodeName];
   }
 
   addChildWithTiedState(ClassName, nodeName, props) {
     this.children[nodeName] = new ClassName(props);
+
+    if (this.events.hasOwnProperty("updateRoot")) {
+      this.children[nodeName].subscribeToEvent(
+        "updateRoot",
+        this.events.updateRoot[0]
+      );
+    }
+
+    if (this.children[nodeName].addChildNodes)
+      this.children[nodeName].addChildNodes();
+
     this.state[nodeName] = this.children[nodeName].state;
 
     return this.children[nodeName];
   }
 
-  addState(state, callback) {
+  addState(state, event, args) {
     this.state = Object.assign(this.state, state);
-    if (typeof callback === "function") callback();
+    if (event) this.emitEvent(event, args);
+
+    return this;
   }
 
   addToChildren(ClassName, nodeListName, props) {
@@ -42,6 +68,12 @@ export default class Component {
     }
 
     this.children[nodeListName].push(child);
+
+    if (this.events.hasOwnProperty("updateRoot")) {
+      child.subscribeToEvent("updateRoot", this.events.updateRoot[0]);
+    }
+
+    if (child.addChildNodes) child.addChildNodes();
 
     return child;
   }
@@ -55,6 +87,13 @@ export default class Component {
     }
 
     this.children[nodeListName].push(child);
+
+    if (this.events.hasOwnProperty("updateRoot")) {
+      child.subscribeToEvent("updateRoot", this.events.updateRoot[0]);
+    }
+
+    if (child.addChildNodes) child.addChildNodes();
+
     this.state[nodeListName].push(child.state);
 
     return child;
@@ -70,19 +109,33 @@ export default class Component {
   }
 
   render(container, renderOptions) {
-    if (renderOptions) render(this.template(), container, renderOptions);
-    else render(this.template(), container);
+    if (renderOptions) {
+      render(this.template(), container, renderOptions);
+    } else {
+      render(this.template(), container);
+    }
   }
 
-  dispatchEvent(event, target) {}
+  emitEvent(eventName, args) {
+    if (this.events[eventName])
+      this.events[eventName].forEach((fn) => fn(args));
+  }
 
-  handleDispatchedEvent(event, source) {}
+  subscribeToEvent(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [];
+      this.events[eventName].push(callback);
+    }
 
-  handleEvent(event, source) {}
-
-  updateState() {}
-
-  addEventHandler() {}
-
-  linkEvent() {}
+    return {
+      unsubscribe: () =>
+        this.events[eventName] &&
+        this.events[eventName].splice(
+          this.events[eventName].indexOf(callback) >>> 0,
+          1
+        ),
+    };
+  }
 }
--- package/src/nqtui.js
+++ package/src/nqtui.js
@@ -1,5 +1,25 @@
 import Component from "./Component.js";
 import { html } from "lit-html";
 import { classMap } from "lit-html/directives/class-map.js";
+import { styleMap } from "lit-html/directives/style-map.js";
+import { guard } from "lit-html/directives/guard.js";
+import { cache } from "lit-html/directives/cache.js";
+import { repeat } from "lit-html/directives/repeat.js";
+import { live } from "lit-html/directives/live.js";
+import { ifDefined } from "lit-html/directives/if-defined.js";
+import { ref } from "lit-html/directives/ref.js";
+import { until } from "lit-html/directives/until.js";
 
-export { Component, html, classMap };
+export {
+  Component,
+  html,
+  classMap,
+  styleMap,
+  guard,
+  cache,
+  repeat,
+  live,
+  ifDefined,
+  ref,
+  until,
+};
